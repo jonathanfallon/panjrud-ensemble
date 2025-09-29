fc03fa7cdca230bdea62e9a41a939a26cfad9457
Jonathan Fallon <jonathan@scopopop.com>
2025-09-29 12:17:20 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Development flake for panjrud-ensemble";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          name = "panjrud-ensemble-dev";
+
+          buildInputs = with pkgs; [
+            nodejs_24
+            pnpm
+            git
+            curl
+          ];
+
+          shellHook = ''
+            export NODE_OPTIONS="--max-old-space-size=4096"
+          '';
+        };
+      });
+}
